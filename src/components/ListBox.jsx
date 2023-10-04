875148b6b5bfee9ce324a4071c1e4787ca3826a6
bernard rayoso <rayosobernard@gmail.com>
2023-10-05 02:28:06 +0800

    Create MovieList, Moive and Listbox Component

--- src/components/ListBox.jsx
+++ src/components/ListBox.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import MovieList from "./MovieList";
 
 const ListBox = ({ movies }) => {
   const [isOpen1, setIsOpen1] = useState(true);
@@ -8,22 +9,7 @@ const ListBox = ({ movies }) => {
       <button className='btn-toggle' onClick={() => setIsOpen1(open => !open)}>
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && (
-        <ul className='list'>
-          {movies?.map(movie => (
-            <li key={movie.imdbID}>
-              <img src={movie.Poster} alt={`${movie.Title} poster`} />
-              <h3>{movie.Title}</h3>
-              <div>
-                <p>
-                  <span>🗓</span>
-                  <span>{movie.Year}</span>
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
+      {isOpen1 && <MovieList movies={movies} />}
     </div>
   );
 };
