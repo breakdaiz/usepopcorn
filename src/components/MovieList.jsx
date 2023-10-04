3d6f791066175b24dd4b6cfee749df7a68470f22
bernard rayoso <rayosobernard@gmail.com>
2023-10-05 03:43:07 +0800

    Created MovieList, Watchbox, Watched Summary components

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,7 +5,7 @@ const MovieList = ({ movies }) => {
   return (
     <ul className='list'>
       {movies?.map(movie => (
-        <Movie movie={movie} />
+        <Movie key={movie.imdbID} movie={movie} />
       ))}
     </ul>
   );
